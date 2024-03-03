a531d1aa8693da40ad3976a423f252041f264b6c
SinthuPriya <sinthuja.engg@gmail.com>
2024-03-03 17:47:19 +0530

    Taj1

--- src/org/test/TajHotelMumbai.java
+++ src/org/test/TajHotelMumbai.java
@@ -1,5 +1,11 @@
 package org.test;
 
 public class TajHotelMumbai {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Taj mahal is a beautiful place");
+		System.out.println("It has a beautiful location");
+	}
 
 }

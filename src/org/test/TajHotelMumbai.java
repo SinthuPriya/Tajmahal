0d7a25dc6a2de9b0a7b0e1aa32cd7150c87ab22e
SinthuPriya <sinthuja.engg@gmail.com>
2024-03-03 17:57:18 +0530

    Taj2

--- src/org/test/TajHotelMumbai.java
+++ src/org/test/TajHotelMumbai.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class TajHotelMumbai {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(" My favourite destination is taj mahal");
+		System.out.println(" My favourite place is delhi");
+		
+	}
 
 }
